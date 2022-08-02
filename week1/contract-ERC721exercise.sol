dc4624fb72b17f0d1f73c6ccc5295d4d0ec15f0b
JimS <jimsun6428@gmail.com>
2022-08-02 14:12:16 +0800

    use mapping to limit every msg sender mint count

--- week1/contract-ERC721exercise.sol
+++ week1/contract-ERC721exercise.sol
@@ -12,12 +12,18 @@ contract SunsetJ17 is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
 
     Counters.Counter private _tokenIdCounter;
     uint256 MAX_SUPPLY = 10000;
+    uint256 MAX_MINT_COUNT = 5;
+
+    // declare a map for every possible address
+    mapping(address => uint) public mintCount;
 
     constructor() ERC721("SunsetJ17", "SUS") {}
 
     function safeMint(address to, string memory uri) public {
         require(_tokenIdCounter.current() <= MAX_SUPPLY, "sorry we have reached the cap");
+        require(mintCount[msg.sender] <= MAX_MINT_COUNT, "You have already minted the maximun number of NFT");
         uint256 tokenId = _tokenIdCounter.current();
+        mintCount[msg.sender]++;
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
