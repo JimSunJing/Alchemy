8156fb2267818fe8595eb6e8538897f664e28742
JimS <jimsun6428@gmail.com>
2022-08-31 14:59:13 +0800

    create week 4 nextJS template

---
+++ week4/nftgallery/pages/index.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+ 
+  return (
+    <div className="flex flex-col items-center justify-center py-8 gap-y-3">
+      <div className="flex flex-col w-full justify-center items-center gap-y-2">
+        <input type={"text"} placeholder="Add your wallet address"></input>
+        <input type={"text"} placeholder="Add the collection address"></input>
+      </div>
+    </div>
+  )
+}
+
+export default Home
